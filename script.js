e5ca219762055874a8fa1f86e8389a8fe2985050
caiorx <caiofbar@gmail.com>
2023-07-18 02:21:52 -0300

    inserindo arquivos index e css

---
+++ script.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.text-header', {duration:2000})
+
+sr.reveal('.secao-2', {
+    rotate: { x: 0,  y:80, z: 0 },
+    duration:3000
+});
+
+sr.reveal('footer', {
+    rotate: { x: 0,  y:80, z: 0 },
+    duration:3000
+});
+
